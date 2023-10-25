be70790d848ad37de79434e2e83b8429e641fc1c
Abhinand Krishna AM <am.abhinandkrishna@gmail.com>
2023-10-25 22:34:37 +0530

    hash router

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Header from "./comp/Header";
 import Footer from "./comp/Footer";
 import Top from "./comp/Top";
 // import Top2 from "./comp/Top2";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import React from "react";
 import Signup from "./comp/Singup"; // Correct the typo in "Signup"
 import ReactDOM from "react-dom/client";
@@ -17,7 +17,7 @@ function App() {
     <div className="App">
       
         
-      <Router basename="/newtonn"  >
+      <Router   >
       <Top />
         <Header />
         <Routes>
