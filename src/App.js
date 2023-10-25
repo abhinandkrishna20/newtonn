7afae10a58e971d03860e530f740239d9e73960c
Abhinand Krishna AM <am.abhinandkrishna@gmail.com>
2023-10-25 23:25:23 +0530

    base edit

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
         
       <HashRouter   >
       <Top />
-        <Header  basename="/newtonn" />
+        <Header  basename="/newtonn/" />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/sign" element={<Signup />} />
